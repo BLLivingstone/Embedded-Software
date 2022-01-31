8a5bf3089a1a293c0ba92b147a80602355fc0d5f
BLLivingstone <78822389+BLLivingstone@users.noreply.github.com>
2022-01-31 16:15:57 +0000

    Update Assignment_1.ino

--- Assignment_1/Assignment_1.ino
+++ Assignment_1/Assignment_1.ino
@@ -1,3 +1,9 @@
+#define int a = 1200   //L  micro
+#define int b = 900    //I  micro
+#define int c = 9      //V
+#define int d = 4500   //I  micro
+#define int mod = 2    //N
+
 void setup() {
   // put your setup code here, to run once:
 
