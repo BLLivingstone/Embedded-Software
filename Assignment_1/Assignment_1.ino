c4f25540276c222d407285ed8cb5ce758c437166
BLLivingstone <78822389+BLLivingstone@users.noreply.github.com>
2022-02-10 00:31:40 +0000

    Update Assignment_1.ino
    preliminary code planning

--- Assignment_1/Assignment_1.ino
+++ Assignment_1/Assignment_1.ino
@@ -2,12 +2,39 @@
 #define int b = 900    //I  micro
 #define int c = 9      //V
 #define int d = 4500   //I  micro
-#define int mod = 2    //N
+#define int mod = 2    //N  Generate inverted form of complete Sig A waveform (from the largest pulse to the shortest) until switch 2 set back to 0 
 
 void setup() {
   // put your setup code here, to run once:
+  
+  //set buttons to input
+  
+  //set signal pins to output
+
+  //set buttons to rising edge
+
+  //printf("\n pin set")
 
 }
+//void function to invert wave form{
+//  if button 1 pressed
+//    true
+//  else
+//    false
+//    printf("/n button 1 pressed")
+//
+//  if button 2 pressed
+//    true
+//  else
+//    false
+//    printf("/n button 2 pressed")
+//}
+
+//void function_name(){
+////function 
+//for int (i=0;i>1; i++)
+//  x=+;
+//}
 
 void loop() {
   // put your main code here, to run repeatedly:
