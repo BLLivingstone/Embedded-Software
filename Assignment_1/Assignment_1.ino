1a2355a61ca1b0adaedb70d21d20d71f9921ac87
BLLivingstone <78822389+BLLivingstone@users.noreply.github.com>
2022-02-10 21:27:50 +0000

    Update Assignment_1.ino

--- Assignment_1/Assignment_1.ino
+++ Assignment_1/Assignment_1.ino
@@ -3,20 +3,30 @@
 #include <string.h>
 
 //#define pins
+#define SIGNAL_A 15
+#define SIGNAL_B 21
+#define BUTTON_1 22
+#define BUTTON_2 23
 
 //parameters for waveform
-#define int a = 1200;   //L  micro
-#define int b = 900;   //I  micro
-#define int c = 9;      //V
-#define int d = 4500;   //I  micro
-#define int systemMode = 2;    //N  Generate inverted form of complete Sig A waveform (from the largest pulse to the shortest) until switch 2 set back to 0 
+#define PARAM_A 1200     //L  micro
+#define PARAM_B 900      //I  micro
+#define PARAM_C 13       //V
+#define PARAM_D 4500     //I  micro
+//system_mode = 2        //N  Generate inverted form of complete Sig A waveform (from the largest pulse to the shortest) until switch 2 set back to 0 
+
+
+bool button = false; // creates button variable as boolean, this will help with inverting waveform untill button 2 is back to zero
+bool button2 = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  
   //set buttons to input
+  pinMode(BUTTON_1, INPUT);
+  pinMode(BUTTON_2, INPUT);
   
   //set signal pins to output
+  pinMode(SIGNAL_A, OUTPUT);
+  pinMode(SIGNAL_B, OUTPUT);
 
   //set buttons to rising edge
 
@@ -37,15 +47,15 @@ void setup() {
 //    printf("/n button 2 pressed")
 //}
 
-//void function_name(){
+void function_name(){
 //function 
-//for int (i=0;i>10; i++)
+//for int (i=0;i<param_c; i++)
 //  count=+;
 //
-//if a, high
+//if param_a, high
 //  width+(50*count);
 //
-//}
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
