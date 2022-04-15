70b66ff48c3de2971328db1db223272a6fced157
BLLivingstone <78822389+BLLivingstone@users.noreply.github.com>
2022-04-15 18:38:39 +0100

    Update Assignment_3.ino

--- Assignment_3/Assignment_3.ino
+++ Assignment_3/Assignment_3.ino
@@ -35,17 +35,24 @@ void task_7(void *pvParameters);
 void task_8(void *pvParameters);
 void task_9(void *pvParameters);
 
-bool button;
 bool button_pressed;
 
 int error_code;
 
-float average_analogue_in;
 float analog_in;
 float analog[4];
 float frequency_in;
 float wave_form;
 
+static QueueHandle_t a_queue;
+static SemaphoreHandle_t data_protc;
+
+struct data_for_tasks{
+  bool button = false;
+  float frequency_in;
+  float average_analogue_in;
+};
+data_for_tasks data_t;
 
 void setup() {
   Serial.begin(57600);
@@ -62,6 +69,78 @@ void setup() {
   analog[1] =0;
   analog[2] =0;
   analog[3] =0;
+
+  xTaskCreate(
+        task_1,
+        "task 1", // name for readability 
+        512,      // stack size
+        NULL,
+        4,        //priority
+        NULL);
+
+    xTaskCreate(
+        task_2,
+        "task 2",
+        512,
+        NULL,
+        3,
+        NULL);
+
+    xTaskCreate(
+        task_3,
+        "task 3",
+        1024,
+        NULL,
+        3,
+        NULL);
+
+    xTaskCreate(
+        task_4,
+        "task 4",
+        1024,
+        NULL,
+        2,
+        NULL);
+
+    xTaskCreate(
+        task_5,
+        "task 5",
+        1024,
+        NULL,
+        1,
+        NULL);
+
+    xTaskCreate(
+        task_6,
+        "task 6",
+        512,
+        NULL,
+        3,
+        NULL);
+
+    xTaskCreate(
+        task_7,
+        "task 7",
+        1024,
+        NULL,
+        4,
+        NULL);
+
+    xTaskCreate(
+        task_8,
+        "task 8",
+        512,
+        NULL,
+        3,
+        NULL);
+
+    xTaskCreate(
+        task_9,
+        "task 9",
+        1024,
+        NULL,
+        4,
+        NULL);
 }
 
 void task_1(void *pvParameters){ // rate 5Hz
@@ -69,10 +148,10 @@ void task_1(void *pvParameters){ // rate 5Hz
   //‘Normal’ operation of SigB in Assignment 1). Timings should be within 5%.
   (void) pvParameters;
   for(;;){
-  digitalWrite(WATCH_DOG, HIGH);
-  delayMicroseconds(50);
-  digitalWrite(WATCH_DOG,LOW);
-  vTaskDelay(RATE_TASK_1);
+    digitalWrite(WATCH_DOG, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(WATCH_DOG,LOW);
+    vTaskDelay(RATE_TASK_1);
   }
 }
 void task_2(void *pvParameters){ // rate 1Hz
@@ -80,9 +159,12 @@ void task_2(void *pvParameters){ // rate 1Hz
   //generator for students using Proteus)
   (void) pvParameters;
   for(;;){
-  button = digitalRead(BUTTON_1);
-  button_pressed = button;
-  vTaskDelay(RATE_TASK_2);
+    if(xSemaphoreTake(data_protc, portMAX_DELAY) == pdTRUE){
+      data_t.button = digitalRead(BUTTON_1);
+      button_pressed = data_t.button;
+      xSemaphoreGive(data_protc);
+    }
+      vTaskDelay(RATE_TASK_2);
   }
 }
 void task_3(void *pvParameters){ //rate 24Hz
@@ -91,9 +173,12 @@ void task_3(void *pvParameters){ //rate 24Hz
   //duty cycle). Accuracy to 2.5% is acceptable.
   (void) pvParameters;
   for(;;){
-  wave_form = pulseIn(PULSE_IN, LOW); // returns the length of pulse in micro seconds
-  frequency_in = 1000000.0/(wave_form*2); // conversion for seconds
-  vTaskDelay(RATE_TASK_3);
+    if(xSemaphoreTake(data_protc, portMAX_DELAY) == pdTRUE){
+      wave_form = pulseIn(PULSE_IN, LOW); // returns the length of pulse in micro seconds
+      data_t.frequency_in = 1000000.0/(wave_form*2); // conversion for seconds
+      xSemaphoreGive(data_protc);
+    }
+      vTaskDelay(RATE_TASK_3);
   }
 }
 void task_4(void *pvParameters){ //rate 24Hz 
@@ -101,23 +186,38 @@ void task_4(void *pvParameters){ //rate 24Hz
   //of 3.3V, using a potentiometer.
   (void) pvParameters;
   for(;;){
-  for (int i=1;i<4;i++){
-    analog[i-1] = analog[i];
-  }
-  analog[2] = analogRead(ANALOG_IN);
-  vTaskDelay(RATE_TASK_4);
+    for (int i=1;i<4;i++){
+      analog[i-1] = analog[i];
+    }
+    analog[2] = analogRead(ANALOG_IN);
+    vTaskDelay(RATE_TASK_4);
   }
 }
 void task_5(void *pvParameters){ //rate 10Hz
   //Compute filtered analogue value, by averaging the last 4 readings. 
-  average_analogue_in=0;
+  data_t.average_analogue_in=0;
   (void) pvParameters;
   for(;;){
-  for(int i=1;i<4;i++){
-    average_analogue_in = average_analogue_in + analog[i];
-  }
-  average_analogue_in =average_analogue_in / 4;
-  vTaskDelay(RATE_TASK_5);
+    int c = 0;
+    if(xQueueReceive(a_queue, &c, 100)){
+        for (int i = 1; i < 4; i++)
+        {
+            analogs[i - 1] = analogs[i];
+        }
+
+        analogs[3] = x * (3.3 / 4095);
+    }
+        
+    if(xSemaphoreTake(data_protc, portMAX_DELAY) == pdTRUE){
+      for(int i=1;i<4;i++){
+        data_t.average_analogue_in = data_t.average_analogue_in + analog[i];
+      }
+      
+      data_t.average_analogue_in = data_t.average_analogue_in / 4;
+      xQueueSend(a_queue, &c, 100);
+      xSemaphoreGive(data_protc);
+    }
+      vTaskDelay(RATE_TASK_5);
   }
 }
 void task_6(void *pvParameters){ //rate 10Hz
@@ -126,30 +226,34 @@ void task_6(void *pvParameters){ //rate 10Hz
   //multiple loops (e.g. to be executed in different slots of the cyclic executive). 
   (void) pvParameters;
   for(;;){
-  for ( int i=0; i<1000; i++){
-    __asm__ __volatile__("nop");
-  }
-  vTaskDelay(RATE_TASK_6);
+    for ( int i=0; i<1000; i++){
+      __asm__ __volatile__("nop");
+    }
+    vTaskDelay(RATE_TASK_6);
   }
 }
 void task_7(void *pvParameters){ //rate 3Hz
   //Perform the following check:
   (void) pvParameters;
   for(;;){
-  if(average_analogue_in >(3.3/2)){ //3.3/2 is half maximum range for analogue input
-      error_code = 1;
-  }
-  else{
-      error_code = 0;
-  }
-  vTaskDelay(RATE_TASK_7);
+
+    if (xQueueReceive(average_queue, &x, 100)){
+    
+      if(a_queue >(3.3/2)){ //3.3/2 is half maximum range for analogue input
+          error_code = 1;
+      }
+      else{
+          error_code = 0;
+      }
+    }
+    vTaskDelay(RATE_TASK_7);
   }
 } 
 void task_8(void *pvParameters){ //rate 3Hz
   (void) pvParameters;
   for(;;){
-  digitalWrite(LED,error_code);
-  vTaskDelay(RATE_TASK_8);
+    digitalWrite(LED,error_code);
+    vTaskDelay(RATE_TASK_8);
   }
 }
 
@@ -161,16 +265,16 @@ void task_9(void *pvParameters){ //rate 0.2Hz
 //    Filtered analogue input.
   (void) pvParameters;
   for(;;){
-  if(button_pressed == false){
-    //serial print task 2 switch button_pressed
-    Serial.print(button);
-    Serial.print(", \t");
-    Serial.print(frequency_in);
-    Serial.print(", \t");
-    Serial.print(average_analogue_in);
-    Serial.print("\n");
-   }
-   vTaskDelay(RATE_TASK_9);
+    if(button_pressed == false){
+      //serial print task 2 switch button_pressed
+      Serial.print(data_t.button);
+      Serial.print(", \t");
+      Serial.print(data_t.frequency_in);
+      Serial.print(", \t");
+      Serial.print(data_t.average_analogue_in);
+      Serial.print("\n");
+     }
+    vTaskDelay(RATE_TASK_9);
   }
 }
 void loop(){} // only needed for code to compile
